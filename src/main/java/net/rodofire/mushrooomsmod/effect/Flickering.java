9d0b9fb36091bf5d15ed3187c33dbb77eafe1139
RodoFire <louisbriche@gmail.com>
2024-03-05 18:20:08 +0100

    added new effect for later

--- src/main/java/net/rodofire/mushrooomsmod/effect/Flickering.java
+++ src/main/java/net/rodofire/mushrooomsmod/effect/Flickering.java
@@ -1,16 +1,8 @@
 package net.rodofire.mushrooomsmod.effect;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.util.Window;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.nbt.NbtCompound;
-
 public class Flickering extends StatusEffect {
     protected Flickering(StatusEffectCategory category, int color) {
         super(category, color);
