c766b1054fe3a99bbcf6c254f835bf8a903bfee2
RodoFire <louisbriche@gmail.com>
2024-03-10 22:19:20 +0100

    blue luminescent vine feature finished

--- src/main/java/net/rodofire/mushrooomsmod/world/BlueLuminescentVinesFeature.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/BlueLuminescentVinesFeature.java
@@ -1,6 +1,8 @@
 package net.rodofire.mushrooomsmod.world;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.block.AbstractPlantStemBlock;
+import net.minecraft.registry.tag.BlockTags;
 import net.rodofire.mushrooomsmod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
@@ -9,6 +11,8 @@ import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.TwistingVinesFeatureConfig;
 import net.minecraft.world.gen.feature.util.FeatureContext;
+import net.rodofire.mushrooomsmod.block.custom.BlueLuminescentVines;
+import net.rodofire.mushrooomsmod.util.ModTags;
 
 public class BlueLuminescentVinesFeature extends Feature<TwistingVinesFeatureConfig> {
     public BlueLuminescentVinesFeature(Codec<TwistingVinesFeatureConfig> configCodec) {
@@ -17,23 +21,34 @@ public class BlueLuminescentVinesFeature extends Feature<TwistingVinesFeatureCon
 
     @Override
     public boolean generate(FeatureContext context) {
-        BlockPos pos=context.getOrigin();
+        BlockPos pos = context.getOrigin();
         StructureWorldAccess world = context.getWorld();
-        int randomheight = Random.create().nextBetween(3,50);
 
-        int maxheight = canGenerate(randomheight,pos,world);
-        if(maxheight!=randomheight)randomheight = Random.create().nextBetween(3,maxheight);
+        int randomheight = Random.create().nextBetween(3, 50);
+        int maxheight = canGenerate(randomheight, pos, world);
+        int state;
 
-        for (int i =0; i<=randomheight;++i){
-            world.setBlockState(pos, ModBlocks.CAERULEA_VOLUBILIS.getDefaultState(), Block.NOTIFY_LISTENERS);
+        if (maxheight != randomheight) randomheight = Random.create().nextBetween(3, maxheight);
+
+
+        for (int i = 0; i <= randomheight; ++i) {
+            pos = pos.up();
+            state = randomheight - i;
+            if (state == randomheight) state = 4;
+            else if (state == randomheight - 1) state = 3;
+            else if (state == randomheight - 2) state = 2;
+            else if (state == 0) state = 0;
+            else state = 1;
+            world.setBlockState(pos.down(), ModBlocks.CAERULEA_VOLUBILIS.getDefaultState().with(BlueLuminescentVines.STAGE, state), Block.NOTIFY_NEIGHBORS);
         }
         return true;
     }
 
-    public static int canGenerate(int height, BlockPos pos,StructureWorldAccess world){
-        for (int i=0;i<height;++i){
+    public static int canGenerate(int height, BlockPos pos, StructureWorldAccess world) {
+        for (int i = 0; i < height; ++i) {
             pos = pos.up();
-            if(world.isAir(pos.down())) continue;
+            if (world.isAir(pos.down()) || world.getBlockState(pos.down()).isIn(BlockTags.LEAVES) || world.getBlockState(pos.down()).isOf(ModBlocks.CAERULEA_VOLUBILIS))
+                continue;
             return i;
         }
         return height;
