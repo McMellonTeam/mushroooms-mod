b077a7fd5ae01f601d1fb74835ecee88d248d523
RodoFire <louisbriche@gmail.com>
2024-03-27 13:48:09 +0100

    trying to solve blue vines generation bug

--- src/main/java/net/rodofire/mushrooomsmod/world/BlueLuminescentVinesFeature.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/BlueLuminescentVinesFeature.java
@@ -1,10 +1,8 @@
 package net.rodofire.mushrooomsmod.world;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.block.AbstractPlantStemBlock;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.gen.feature.FeatureConfig;
 import net.rodofire.mushrooomsmod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
@@ -14,7 +12,6 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.TwistingVinesFeatureConfig;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 import net.rodofire.mushrooomsmod.block.custom.BlueLuminescentVines;
-import net.rodofire.mushrooomsmod.util.ModTags;
 
 public class BlueLuminescentVinesFeature extends Feature<TwistingVinesFeatureConfig> {
     public BlueLuminescentVinesFeature(Codec<TwistingVinesFeatureConfig> configCodec) {
@@ -31,15 +28,27 @@ public class BlueLuminescentVinesFeature extends Feature<TwistingVinesFeatureCon
 
         int randomheight = Random.create().nextBetween(3, config.maxHeight());
         int maxheight = canGenerate(randomheight, secondmutable.set(pos), world);
-        int state;
+        int i = config.spreadWidth();
 
         if (maxheight <= randomheight) randomheight = Random.create().nextBetween(0, maxheight);
 
         mutable.set(pos);
+        for (int a=0; a<=i*i; ++a){
+            mutable.set(pos).move(Random.create().nextBetween(-i,i),Random.create().nextBetween(3, config.maxHeight()), Random.create().nextBetween(-i,i) );
+            if (canGenerate(randomheight, secondmutable.set(pos), world)==0)continue;
+            if (maxheight <= randomheight) randomheight = Random.create().nextBetween(0, maxheight);
+            generateColumn(randomheight, world, mutable);
+        }
 
+
+        return true;
+    }
+
+    private static void generateColumn(int randomheight, StructureWorldAccess world, BlockPos.Mutable mutable) {
+        int state;
         if (randomheight <= 4) {
             for (int i = 0; i < randomheight; ++i) {
-                state = randomheight - i-1;
+                state = randomheight - i - 1;
                 world.setBlockState(mutable, ModBlocks.CAERULEA_VOLUBILIS.getDefaultState().with(BlueLuminescentVines.STAGE, state), Block.NOTIFY_LISTENERS);
                 mutable.move(Direction.UP);
             }
@@ -55,17 +64,20 @@ public class BlueLuminescentVinesFeature extends Feature<TwistingVinesFeatureCon
                 mutable.move(Direction.UP);
             }
         }
-        return true;
     }
 
     public static int canGenerate(int height, BlockPos.Mutable mutable, StructureWorldAccess world) {
+        if (canBePlaced(mutable, world)) return 0;
         for (int i = 0; i < height; ++i) {
             mutable.move(Direction.UP);
-            if (world.isAir(mutable.down()) || world.getBlockState(mutable.down()).isIn(BlockTags.LEAVES) || world.getBlockState(mutable.down()).isOf(ModBlocks.CAERULEA_VOLUBILIS))
-                continue;
+            if (world.isAir(mutable)) continue;
             return i;
         }
         return height;
+    }
+
+    public static boolean canBePlaced(BlockPos.Mutable mutable, StructureWorldAccess world) {
+        return world.getBlockState(mutable).isOf(ModBlocks.BLUE_LUMINESCENT_SCHROOM_DEEPSLATE);
     }
 
 }
