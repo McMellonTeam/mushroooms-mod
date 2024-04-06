6d6637880de684f8751de5a954c9ab746d8f3136
RodoFire <louisbriche@gmail.com>
2024-04-06 18:55:28 +0200

    added useful class for wood

---
+++ src/main/java/net/rodofire/mushrooomsmod/block/BlockUtils.java
@@ -0,0 +1,21 @@
+package net.rodofire.mushrooomsmod.block;
+
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+
+public class BlockUtils {
+    public static void registerStripable(){
+        StrippableBlockRegistry.register(ModBlocks.BLUE_LUMINESCENT_LOG, ModBlocks.BLUE_LUMINESCENT_STRIPPED_LOG);
+        StrippableBlockRegistry.register(ModBlocks.BLUE_LUMINESCENT_WOOD, ModBlocks.BLUE_LUMINESCENT_STRIPPED_WOOD);
+    }
+    public static void registerFlammable(){
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_LOG,5,5);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_STRIPPED_LOG,5,5);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_WOOD,5,5);
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_STRIPPED_WOOD,5,5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_PLANKS,5,20);
+
+        FlammableBlockRegistry.getDefaultInstance().add(ModBlocks.BLUE_LUMINESCENT_LEAVES,25,50);
+    }
+}
