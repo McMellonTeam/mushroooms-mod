ba7471f558af3379b0997afff447d9bec4c43671
RodoFire <louisbriche@gmail.com>
2024-04-08 23:13:51 +0200

    remove debugging line

--- src/main/java/net/rodofire/mushrooomsmod/item/Custom/HammerItem.java
+++ src/main/java/net/rodofire/mushrooomsmod/item/Custom/HammerItem.java
@@ -52,7 +52,6 @@ public class HammerItem extends ToolItem {
     @Override
     public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
         if (hammeruse != 0 || world.isClient()) return mine(world, pos);
-        System.out.println(hammeruse);
         return use(world, pos,miner);
     }
 
