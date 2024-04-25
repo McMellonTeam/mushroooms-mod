02f2329553d90cbc1e21e5cc7c6f4dd226875c1f
RodoFire <louisbriche@gmail.com>
2024-04-25 15:47:05 +0200

    crystal finally working properlly

--- src/main/java/net/rodofire/mushrooomsmod/block/custom/Crystal.java
+++ src/main/java/net/rodofire/mushrooomsmod/block/custom/Crystal.java
@@ -4,6 +4,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.IntProperty;
@@ -13,11 +16,15 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import net.rodofire.mushrooomsmod.block.ModBlocks;
+import org.jetbrains.annotations.Nullable;
 
 public class Crystal extends Block {
     public static final VoxelShape SHAPE = Block.createCuboidShape(3, 0, 3, 13, 16, 13);
+    public static final DirectionProperty VERTICAL_DIRECTION = Properties.VERTICAL_DIRECTION;
+    public static final IntProperty STAGE = Properties.AGE_1;
 
     public Crystal(Settings settings) {
         super(settings);
@@ -28,52 +35,79 @@ public class Crystal extends Block {
         return SHAPE;
     }
 
-    public static final DirectionProperty VERTICAL_DIRECTION = Properties.VERTICAL_DIRECTION;
-    public static final IntProperty STAGE = Properties.AGE_1;
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(STAGE, VERTICAL_DIRECTION);
     }
 
+    public boolean canPlace(World world, BlockPos pos, BlockState state) {
+        BlockState blockState = world.getBlockState(pos);
+        Direction direction = state.get(VERTICAL_DIRECTION);
+        Direction direction2 = direction.getOpposite();
+        System.out.println(blockState);
+        if (blockState.isOf(ModBlocks.BLUE_CRYSTAL)) direction2 = blockState.get(VERTICAL_DIRECTION);
+        return blockState.equals(state) || direction2 == direction || blockState.isSideSolidFullSquare(world, pos, state.get(VERTICAL_DIRECTION));
+    }
+
+    public Direction getDirection(BlockState state, World world, BlockPos pos) {
+        if (canPlace(world, pos.down(), state)) return Direction.UP;
+        else if (canPlace(world, pos.up(), state)) return Direction.DOWN;
+        return null;
+    }
 
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        Direction direction = getDirection((World) world, pos, state.get(VERTICAL_DIRECTION));
-        return canPlace(direction, world, pos, state);
-    }
-
-    public boolean canPlace(Direction direction, WorldView world, BlockPos pos, BlockState state) {
-        if (direction == null) return false;
-        BlockState blockState = world.getBlockState(pos.offset(direction.getOpposite()));
-        System.out.println(blockState+" "+state);
-        return state.equals(blockState) || blockState.isOpaqueFullCube(world, pos);
-    }
-
-    public Direction getDirection(World world, BlockPos pos, Direction direction) {
-        Direction direction2;
-        if (canPlace(direction, world, pos, world.getBlockState(pos))) {
-            direction2 = direction;
-        } else if (canPlace(direction.getOpposite(), world, pos, world.getBlockState(pos))) {
-            direction2 = direction.getOpposite();
-        } else {
-            return null;
-        }
-        return direction2;
+        Direction direction = getDirection(state, (World) world, pos);
+        return direction == Direction.UP || direction == Direction.DOWN;
     }
 
     @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
-        System.out.println(state);
-        if (!canPlaceAt(state, world, pos)) world.setBlockState(pos, Blocks.AIR.getDefaultState());
-        setBlockstate(world, pos, state, state.get(VERTICAL_DIRECTION));
-        super.neighborUpdate(state, world, pos, sourceBlock, sourcePos, notify);
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        Direction direction2 = state.get(VERTICAL_DIRECTION);
+        if (!canPlace((World) world, pos.offset(direction2.getOpposite()), state)){
+            System.out.println("cant place");
+            return Blocks.AIR.getDefaultState();
+        }
+        if (world.getBlockState(pos.offset(direction)).isOf(ModBlocks.BLUE_CRYSTAL)){
+            return state;
+        }
+        System.out.println("place stage 0");
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
-    public void setBlockstate(World world, BlockPos pos, BlockState state, Direction dir) {
-        if (world.getBlockState(pos.offset(dir.getOpposite())).getBlock() == this) {
-            world.setBlockState(pos, state.with(STAGE, 1).with(VERTICAL_DIRECTION, getDirection(world, pos, dir)));
-        } else world.setBlockState(pos, state.with(STAGE, 0).with(VERTICAL_DIRECTION, getDirection(world, pos, dir)));
+    /*@Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        if (!world.isClient) return;
+        Direction direction = getDirection(state, (World) world, pos);
+        if (direction == null) return;
+        if (world.getBlockState(pos.offset(direction)).getBlock() == ModBlocks.BLUE_CRYSTAL)
+            world.setBlockState(pos.offset(direction), state.with(VERTICAL_DIRECTION, direction).with(STAGE, 1), 1);
+        world.setBlockState(pos, state.with(VERTICAL_DIRECTION, direction).with(STAGE, 0), 1);
+    }*/
+
+    public void updateState(World world, BlockPos pos, BlockState state, Direction direction) {
+        if (world.getBlockState(pos.offset(direction.getOpposite())).isOf(ModBlocks.BLUE_CRYSTAL)) {
+            world.setBlockState(pos.offset(direction.getOpposite()), state.with(STAGE, 1).with(VERTICAL_DIRECTION, direction));
+        }
     }
 
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        World world = ctx.getWorld();
+        Direction direction = ctx.getVerticalPlayerLookDirection().getOpposite();
+        BlockPos pos = ctx.getBlockPos();
+        BlockState blockState = ModBlocks.BLUE_CRYSTAL.getDefaultState();
+        System.out.println("getPlacementState");
+        if (canPlace(world, pos.offset(direction.getOpposite()), blockState.with(VERTICAL_DIRECTION, direction))) {
+            updateState(world, pos, blockState, direction);
+            return blockState.with(VERTICAL_DIRECTION, direction).with(STAGE, 0);
+        }
+        else if (canPlace(world, pos.offset(direction), blockState.with(VERTICAL_DIRECTION, direction.getOpposite()))) {
+            updateState(world, pos, blockState, direction.getOpposite());
+            return blockState.with(VERTICAL_DIRECTION, direction.getOpposite()).with(STAGE, 0);
+        }
+        return null;
+    }
 }
