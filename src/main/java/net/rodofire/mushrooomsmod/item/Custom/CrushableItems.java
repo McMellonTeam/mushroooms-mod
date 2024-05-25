bdc1a212ff4f74cbbda18aa0237b3931a18f13a8
RodoFire <louisbriche@gmail.com>
2024-05-25 23:25:58 +0200

    removed unnecessary imports and added tooltip

--- src/main/java/net/rodofire/mushrooomsmod/item/Custom/CrushableItems.java
+++ src/main/java/net/rodofire/mushrooomsmod/item/Custom/CrushableItems.java
@@ -1,9 +1,17 @@
 package net.rodofire.mushrooomsmod.item.Custom;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.rodofire.mushrooomsmod.item.ModItems;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CrushableItems extends Item {
     Item crushed;
@@ -14,8 +22,14 @@ public class CrushableItems extends Item {
     }
 
     public static ItemStack getCrushed(ItemStack stack) {
+        Item item = stack.getItem();
         if (stack.getItem().equals(Items.DIAMOND)) return ModItems.CRUSHED_DIAMOND.asItem().getDefaultStack();
         return null;
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.mushrooomsmod.crushable_items"));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
 }
