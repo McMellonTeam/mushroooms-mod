056f90f1a8a79a1f83d7c29239e43f625fbdb967
RodoFire <louisbriche@gmail.com>
2024-06-04 13:04:18 +0200

    crystal creeper registration

--- src/main/java/net/rodofire/mushrooomsmod/entity/ModEntities.java
+++ src/main/java/net/rodofire/mushrooomsmod/entity/ModEntities.java
@@ -8,6 +8,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.rodofire.mushrooomsmod.MushrooomsMod;
+import net.rodofire.mushrooomsmod.entity.custom.CrystalCreeperEntity;
 import net.rodofire.mushrooomsmod.entity.custom.GrokiEntity;
 import net.rodofire.mushrooomsmod.entity.custom.BoleteCowEntity;
 import net.rodofire.mushrooomsmod.entity.custom.PlotiEntity;
@@ -24,5 +25,9 @@ public class ModEntities {
     public static final EntityType<PlotiEntity> PLOTI = Registry.register(Registries.ENTITY_TYPE,
             new Identifier(MushrooomsMod.MOD_ID, "ploti"), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, PlotiEntity::new)
                     .dimensions(EntityDimensions.fixed(0.25f, 0.3f)).build());
+
+    public static final EntityType<CrystalCreeperEntity> CRYSTAL_CREEPER = Registry.register(Registries.ENTITY_TYPE,
+            new Identifier(MushrooomsMod.MOD_ID, "crystal_creeper"), FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CrystalCreeperEntity::new)
+                    .dimensions(EntityDimensions.fixed(0.6f, 1.7f)).build());
 
 }
