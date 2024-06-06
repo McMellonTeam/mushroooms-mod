438727cef6a8d57285dafda4e91ce3427e8df149
RodoFire <louisbriche@gmail.com>
2024-06-06 22:56:55 +0200

    more WorldGenUtil functions

--- src/main/java/net/rodofire/mushrooomsmod/util/WorldGenUtil.java
+++ src/main/java/net/rodofire/mushrooomsmod/util/WorldGenUtil.java
@@ -1,6 +1,7 @@
 package net.rodofire.mushrooomsmod.util;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
@@ -8,11 +9,26 @@ import net.minecraft.world.StructureWorldAccess;
 
 public class WorldGenUtil {
     public static void generateCircle(StructureWorldAccess world, Random random, int largex, BlockState state, BlockPos pos) {
-        generateOval(world, random, largex, largex, state, pos);
+        generateOval(world, random, largex, largex, state, pos, false);
     }
 
-    public static void generateOval(StructureWorldAccess world, Random random, int largex, int largey, BlockState state, BlockPos pos) {
-        for (int i = 0; )
+    public static void generateOval(StructureWorldAccess world, Random random, int largex, int largez, BlockState state, BlockPos pos, boolean force) {
+        int large = Math.max(largex, largez);
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (int i = 0; i<=large; i++) {
+            for (double j = -Math.PI; j<=Math.PI; j+=Math.PI/(4*i)) {
+                double x = largex * ((double) i /large) * Math.cos(j);
+                double z = largez * ((double) i /large) * Math.sin(j);
+                mutable.set(pos, (int) x, 0, (int) z);
+
+                if(!force){
+                    if (world.getBlockState(mutable).isAir())
+                        world.setBlockState(mutable, state, 2);
+                    continue;
+                }
+                world.setBlockState(mutable, state, 2);
+            }
+        }
     }
 
     public static void generateHalfSphere(StructureWorldAccess world, Random random, int largex, BlockPos pos, Direction direction, BlockState state) {
@@ -56,11 +72,11 @@ public class WorldGenUtil {
             for (double j = large1; j <= large2; j += Math.PI / (4 * i)) {
                 for (double k = -height; k <= height2; k += Math.PI / (4 * i)) {
                     //polar coordinates
-                    double x = largex * Math.cos(j) * Math.cos(k);
-                    double z = largez * Math.sin(j) * Math.cos(k);
-                    double y = largey * Math.sin(k);
+                    double x = largex * ((double) i /large) * Math.cos(j) * Math.cos(k);
+                    double z = largez * ((double) i /large) *Math.sin(j) * Math.cos(k);
+                    double y = largey * ((double) i /large) *Math.sin(k);
 
-                    mutable.set(pos, (int) x, (int) z, (int) y);
+                    mutable.set(pos, (int) x, (int) y, (int) z);
                     if (!force) {
                         if (world.getBlockState(mutable).isAir()) {
                             world.setBlockState(mutable, state, 2);
@@ -69,6 +85,135 @@ public class WorldGenUtil {
                     }
                     world.setBlockState(mutable, state, 2);
                 }
+            }
+        }
+    }
+
+    public static Direction getRandomDirection(){
+        return switch (Random.create().nextBetween(0,5)){
+            case 0 -> Direction.WEST;
+            case 1 -> Direction.EAST;
+            case 2 -> Direction.NORTH;
+            case 3 -> Direction.SOUTH;
+            case 4 -> Direction.UP;
+            default -> Direction.DOWN;
+        };
+    }
+
+    public static Direction getRandomVerticalDirection(){
+        return switch (Random.create().nextBetween(0,1)){
+            case 0 -> Direction.UP;
+            default -> Direction.DOWN;
+        };
+    }
+
+    public static Direction getRandomHorizontalDirection(){
+        return switch (Random.create().nextBetween(0,3)){
+            case 0 -> Direction.WEST;
+            case 1 -> Direction.EAST;
+            case 2 -> Direction.NORTH;
+            default -> Direction.SOUTH;
+        };
+    }
+
+    public static int getRandomOpposite(){
+        return (Random.create().nextBetween(0,1) == 0) ? 1 : -1;
+    }
+
+    //based on the Bresenham algorithm
+    public static void drawLine(StructureWorldAccess world, BlockPos start, BlockPos end, BlockState state) {
+        drawLine(world, start, end, state, false);
+    }
+    public static void drawLine(StructureWorldAccess world, BlockPos start, BlockPos end, BlockState state, boolean force) {
+        int x1 = start.getX();
+        int y1 = start.getY();
+        int z1 = start.getZ();
+        int x2 = end.getX();
+        int y2 = end.getY();
+        int z2 = end.getZ();
+
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+        int dz = Math.abs(z2 - z1);
+
+        int xs = x1 < x2 ? 1 : -1;
+        int ys = y1 < y2 ? 1 : -1;
+        int zs = z1 < z2 ? 1 : -1;
+
+        if (dx >= dy && dx >= dz) {
+            int p1 = 2 * dy - dx;
+            int p2 = 2 * dz - dx;
+            while (x1 != x2) {
+                x1 += xs;
+                if (p1 >= 0) {
+                    y1 += ys;
+                    p1 -= 2 * dx;
+                }
+                if (p2 >= 0) {
+                    z1 += zs;
+                    p2 -= 2 * dx;
+                }
+                p1 += 2 * dy;
+                p2 += 2 * dz;
+                BlockPos currentPos = new BlockPos(x1, y1, z1);
+                BlockState currentState = world.getBlockState(currentPos);
+                if (!force) {
+                    if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
+                        world.setBlockState(currentPos, state, 2);
+                    }
+                    continue;
+                }
+                world.setBlockState(currentPos, state, 2);
+            }
+        } else if (dy >= dx && dy >= dz) {
+            int p1 = 2 * dx - dy;
+            int p2 = 2 * dz - dy;
+            while (y1 != y2) {
+                y1 += ys;
+                if (p1 >= 0) {
+                    x1 += xs;
+                    p1 -= 2 * dy;
+                }
+                if (p2 >= 0) {
+                    z1 += zs;
+                    p2 -= 2 * dy;
+                }
+                p1 += 2 * dx;
+                p2 += 2 * dz;
+                BlockPos currentPos = new BlockPos(x1, y1, z1);
+                BlockState currentState = world.getBlockState(currentPos);
+                if (!force) {
+                    if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
+                        world.setBlockState(currentPos, state, 2);
+                    }
+                    continue;
+                }
+                world.setBlockState(currentPos, state, 2);
+            }
+        } else {
+            int p1 = 2 * dy - dz;
+            int p2 = 2 * dx - dz;
+            while (z1 != z2) {
+                z1 += zs;
+                if (p1 >= 0) {
+                    y1 += ys;
+                    p1 -= 2 * dz;
+                }
+                if (p2 >= 0) {
+                    x1 += xs;
+                    p2 -= 2 * dz;
+                }
+                p1 += 2 * dy;
+                p2 += 2 * dx;
+                BlockPos currentPos = new BlockPos(x1, y1, z1);
+                BlockState currentState = world.getBlockState(currentPos);
+                if (!force) {
+                    if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
+                        world.setBlockState(currentPos, state, 2);
+                    }
+                    continue;
+                }
+                world.setBlockState(currentPos, state, 2);
             }
         }
     }
--- src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/BigCrystal.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/BigCrystal.java
@@ -4,10 +4,12 @@ import com.mojang.serialization.Codec;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
+import net.rodofire.mushrooomsmod.util.WorldGenUtil;
 import net.rodofire.mushrooomsmod.world.features.config.ModSimpleBlockFeatureConfig;
 
 public class BigCrystal extends Feature<ModSimpleBlockFeatureConfig> {
@@ -20,7 +22,7 @@ public class BigCrystal extends Feature<ModSimpleBlockFeatureConfig> {
     @Override
     public boolean generate(FeatureContext<ModSimpleBlockFeatureConfig> context) {
         BlockPos pos = context.getOrigin();
-        BlockPos secondpos = pos.add(Random.create().nextBetween(1, 20) * randomOpposite(), Random.create().nextBetween(6, 30) * randomOpposite(), Random.create().nextBetween(1, 20) * randomOpposite());
+        BlockPos secondpos = pos.add(Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(6, 30) *  WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(1, 16) *  WorldGenUtil.getRandomOpposite());
 
         ModSimpleBlockFeatureConfig config = context.getConfig();
         Random random = context.getRandom();
@@ -33,108 +35,27 @@ public class BigCrystal extends Feature<ModSimpleBlockFeatureConfig> {
         BlockPos middlestate = new BlockPos((secondpos.getX() + pos.getX()) / 2 , (secondpos.getY() + pos.getY()) / 2,( secondpos.getZ() + pos.getZ()) / 2);
         BlockState state1 = world.getBlockState(pos);
         BlockState state2 = world.getBlockState(middlestate);
+
+        Direction direction;
+        if(secondpos.getY() - pos.getY() > 0) direction = Direction.DOWN;
+        else direction = Direction.UP;
+
         if (!state2.isAir()) return false;
         if (state1.isAir() && world.getBlockState(mutable.down()).isAir() && world.getBlockState(mutable.up()).isAir()) return false;
+
         //create a round base
         int baselarge = Random.create().nextBetween(2, 5);
-        for (int i = 1; i < baselarge; i++) {
-            for (float j = (float) -Math.PI; j < Math.PI; j += (float) (Math.PI / (6 * i))) {
+        for (int i = 1; i <= baselarge; i++) {
+            for (float j = (float) -Math.PI; j < Math.PI; j += (float) (Math.PI / (4 * i))) {
                 int x = (int) ((i) * Math.cos(j));
                 int z = (int) ((i) * Math.sin(j));
                 mutable.set(pos, x, 0, z);
+                world.setBlockState(mutable, state, 2);
 
-                drawLine(world, mutable, secondpos, state);
+                WorldGenUtil.drawLine(world, mutable, secondpos, state);
             }
         }
+        WorldGenUtil.generateHalfSphere(world, random, baselarge, pos, direction, state);
         return true;
     }
-
-    //based on the Bresenham algorithm
-    private void drawLine(StructureWorldAccess world, BlockPos start, BlockPos end, BlockState state) {
-        int x1 = start.getX();
-        int y1 = start.getY();
-        int z1 = start.getZ();
-        int x2 = end.getX();
-        int y2 = end.getY();
-        int z2 = end.getZ();
-
-        int dx = Math.abs(x2 - x1);
-        int dy = Math.abs(y2 - y1);
-        int dz = Math.abs(z2 - z1);
-
-        int xs = x1 < x2 ? 1 : -1;
-        int ys = y1 < y2 ? 1 : -1;
-        int zs = z1 < z2 ? 1 : -1;
-
-        if (dx >= dy && dx >= dz) {
-            int p1 = 2 * dy - dx;
-            int p2 = 2 * dz - dx;
-            while (x1 != x2) {
-                x1 += xs;
-                if (p1 >= 0) {
-                    y1 += ys;
-                    p1 -= 2 * dx;
-                }
-                if (p2 >= 0) {
-                    z1 += zs;
-                    p2 -= 2 * dx;
-                }
-                p1 += 2 * dy;
-                p2 += 2 * dz;
-                BlockPos currentPos = new BlockPos(x1, y1, z1);
-                BlockState currentState = world.getBlockState(currentPos);
-                if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
-                    world.setBlockState(currentPos, state, 2);
-                }
-            }
-        } else if (dy >= dx && dy >= dz) {
-            int p1 = 2 * dx - dy;
-            int p2 = 2 * dz - dy;
-            while (y1 != y2) {
-                y1 += ys;
-                if (p1 >= 0) {
-                    x1 += xs;
-                    p1 -= 2 * dy;
-                }
-                if (p2 >= 0) {
-                    z1 += zs;
-                    p2 -= 2 * dy;
-                }
-                p1 += 2 * dx;
-                p2 += 2 * dz;
-                BlockPos currentPos = new BlockPos(x1, y1, z1);
-                BlockState currentState = world.getBlockState(currentPos);
-                if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
-                    world.setBlockState(currentPos, state, 2);
-                }
-            }
-        } else {
-            int p1 = 2 * dy - dz;
-            int p2 = 2 * dx - dz;
-            while (z1 != z2) {
-                z1 += zs;
-                if (p1 >= 0) {
-                    y1 += ys;
-                    p1 -= 2 * dz;
-                }
-                if (p2 >= 0) {
-                    x1 += xs;
-                    p2 -= 2 * dz;
-                }
-                p1 += 2 * dy;
-                p2 += 2 * dx;
-                BlockPos currentPos = new BlockPos(x1, y1, z1);
-                BlockState currentState = world.getBlockState(currentPos);
-                if (currentState.isAir() || currentState.isIn(BlockTags.FLOWERS)) {
-                    world.setBlockState(currentPos, state, 2);
-                }
-            }
-        }
-    }
-
-    public int randomOpposite() {
-        return (Random.create().nextBetween(0, 1) == 0) ? 1 : -1;
-    }
-
-
 }
