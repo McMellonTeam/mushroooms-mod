66387cca1ac86987c6ee640913ffd6f079339781
RodoFire <louisbriche@gmail.com>
2024-06-19 20:05:55 +0200

    added big crystal that can have 2 shards

--- src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/BigCrystal.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/BigCrystal.java
@@ -21,27 +21,55 @@ public class BigCrystal extends Feature<ModSimpleBlockFeatureConfig> {
 
     @Override
     public boolean generate(FeatureContext<ModSimpleBlockFeatureConfig> context) {
-        BlockPos pos = context.getOrigin();
-        BlockPos secondpos = pos.add(Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(6, 30) *  WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(1, 16) *  WorldGenUtil.getRandomOpposite());
+        if (Random.create().nextBetween(0,4) == 0) {
+            BlockPos pos = context.getOrigin();
+            BlockPos secondpos = pos.add(Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(6, 30) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite());
+            BlockPos thirdpos = pos.add(Random.create().nextBetween(1,16) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(6,30) * WorldGenUtil.getSign(secondpos.getY()), Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite());
 
-        ModSimpleBlockFeatureConfig config = context.getConfig();
-        Random random = context.getRandom();
-        StructureWorldAccess world = context.getWorld();
-        BlockState state = config.blockprovider.get(random, pos);
-        BlockPos.Mutable mutable = new BlockPos.Mutable();
+            ModSimpleBlockFeatureConfig config = context.getConfig();
+            Random random = context.getRandom();
+            StructureWorldAccess world = context.getWorld();
+            BlockState state = config.blockprovider.get(random, pos);
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
 
-        mutable.set(pos);
+            mutable.set(pos);
 
-        BlockPos middlestate = new BlockPos((secondpos.getX() + pos.getX()) / 2 , (secondpos.getY() + pos.getY()) / 2,( secondpos.getZ() + pos.getZ()) / 2);
-        BlockState state1 = world.getBlockState(pos);
-        BlockState state2 = world.getBlockState(middlestate);
+            BlockPos middlestate = new BlockPos((secondpos.getX() + pos.getX()) / 2, (secondpos.getY() + pos.getY()) / 2, (secondpos.getZ() + pos.getZ()) / 2);
+            BlockPos secondmiddlestate = new BlockPos((thirdpos.getX() + pos.getX()) / 2, (thirdpos.getY() + pos.getY()) / 2, (thirdpos.getZ() + pos.getZ()) / 2);
+            BlockState state1 = world.getBlockState(pos);
+            BlockState state2 = world.getBlockState(middlestate);
+            BlockState state3 = world.getBlockState(secondmiddlestate);
 
+            if(!generatePillar(secondpos, pos, state2, state1, world, mutable, state, random)) return false;
+            return generatePillar(thirdpos, pos, state3, state1, world, mutable, state, random);
+        }
+        else {
+            BlockPos pos = context.getOrigin();
+            BlockPos secondpos = pos.add(Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(6, 30) * WorldGenUtil.getRandomOpposite(), Random.create().nextBetween(1, 16) * WorldGenUtil.getRandomOpposite());
+
+            ModSimpleBlockFeatureConfig config = context.getConfig();
+            Random random = context.getRandom();
+            StructureWorldAccess world = context.getWorld();
+            BlockState state = config.blockprovider.get(random, pos);
+            BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+            mutable.set(pos);
+
+            BlockPos middlestate = new BlockPos((secondpos.getX() + pos.getX()) / 2, (secondpos.getY() + pos.getY()) / 2, (secondpos.getZ() + pos.getZ()) / 2);
+            BlockState state1 = world.getBlockState(pos);
+            BlockState state2 = world.getBlockState(middlestate);
+            return this.generatePillar(secondpos, pos, state2, state1, world, mutable, state, random);
+        }
+    }
+
+    private boolean generatePillar(BlockPos secondpos, BlockPos pos, BlockState state2, BlockState state1, StructureWorldAccess world, BlockPos.Mutable mutable, BlockState state, Random random) {
         Direction direction;
         if(secondpos.getY() - pos.getY() > 0) direction = Direction.DOWN;
         else direction = Direction.UP;
 
         if (!state2.isAir()) return false;
-        if (state1.isAir() && world.getBlockState(mutable.down()).isAir() && world.getBlockState(mutable.up()).isAir()) return false;
+        if (state1.isAir() && world.getBlockState(mutable.down()).isAir() && world.getBlockState(mutable.up()).isAir())
+            return false;
 
         //create a round base
         int baselarge = Random.create().nextBetween(2, 5);
@@ -58,4 +86,5 @@ public class BigCrystal extends Feature<ModSimpleBlockFeatureConfig> {
         WorldGenUtil.generateHalfSphere(world, random, baselarge, pos, direction, state);
         return true;
     }
+
 }
