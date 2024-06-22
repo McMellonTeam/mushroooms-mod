1296bdb2aa8523f87b716a90d80200b54c814daf
RodoFire <louisbriche@gmail.com>
2024-06-22 17:49:36 +0200

    more accurate hud

--- src/main/java/net/rodofire/mushrooomsmod/client/HammerHUDOverlay.java
+++ src/main/java/net/rodofire/mushrooomsmod/client/HammerHUDOverlay.java
@@ -34,38 +34,42 @@ public class HammerHUDOverlay implements HudRenderCallback {
             HitResult hit = player.raycast(5, tickDelta, false);
 
             if (item.getItem() instanceof HammerItem && hit.getType() == HitResult.Type.BLOCK) {
-                Direction direction = player.getHorizontalFacing();
-                Direction direction1 = player.getMovementDirection();
                 World world = player.getWorld();
-                Vec3d seconpos = hit.getPos().offset(direction, 0.1);
-                Vec3d seconpos1 = hit.getPos().offset(direction1, 1.1);
+
+                Vec3d seconpos = client.crosshairTarget.getPos();
 
                 BlockPos.Mutable mutable = new BlockPos.Mutable();
-                BlockPos.Mutable mutable1 = new BlockPos.Mutable();
                 mutable.set(seconpos.x, seconpos.y, seconpos.z);
-                mutable1.set(seconpos1.x, seconpos1.y, seconpos1.z);
 
-                if (world.getBlockState(mutable).getBlock() == ModBlocks.FORGE_BLOCK || world.getBlockState(mutable1).getBlock() == ModBlocks.FORGE_BLOCK) {
-                    int width = client.getWindow().getScaledWidth();
-                    int height = client.getWindow().getScaledHeight();
-
-                    int x = width / 2 - 6;
-                    int y = height / 2 + 10;
-
-                    RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                    RenderSystem.setShaderTexture(0, HAMMER);
-
-
-                    int progress = ((HammerItem) item.getItem()).getHammerUse();
-                    float proportion = 1 - ((float) progress / 200);
-                    int filled = (int) (16 * proportion);
-
-                    drawContext.drawTexture(HAMMER, x, y, 0, 0, 16, 16, 16, 16);
-                    RenderSystem.setShaderTexture(0, HAMMER_FULL);
-                    drawContext.drawTexture(HAMMER_FULL, x, y, 0, 0, filled, 16, 16, 16);
+                //Had to do it like this beacause secondpos wasn't always accurate
+                if (world.getBlockState(mutable).getBlock() == ModBlocks.FORGE_BLOCK
+                        || world.getBlockState(mutable.up()).getBlock() == ModBlocks.FORGE_BLOCK || world.getBlockState(mutable.down()).getBlock() == ModBlocks.FORGE_BLOCK
+                        || world.getBlockState(mutable.north()).getBlock() == ModBlocks.FORGE_BLOCK || world.getBlockState(mutable.south()).getBlock() == ModBlocks.FORGE_BLOCK
+                        || world.getBlockState(mutable.east()).getBlock() == ModBlocks.FORGE_BLOCK || world.getBlockState(mutable.west()).getBlock() == ModBlocks.FORGE_BLOCK) {
+                    drawTexture(client, drawContext, item);
                 }
             }
         }
+    }
+
+    private void drawTexture(MinecraftClient client, DrawContext drawContext, ItemStack item) {
+        int width = client.getWindow().getScaledWidth();
+        int height = client.getWindow().getScaledHeight();
+
+        int x = width / 2 - 6;
+        int y = height / 2 + 10;
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, HAMMER);
+
+
+        int progress = ((HammerItem) item.getItem()).getHammerUse();
+        float proportion = 1 - ((float) progress / 200);
+        int filled = (int) (16 * proportion);
+
+        drawContext.drawTexture(HAMMER, x, y, 0, 0, 16, 16, 16, 16);
+        RenderSystem.setShaderTexture(0, HAMMER_FULL);
+        drawContext.drawTexture(HAMMER_FULL, x, y, 0, 0, filled, 16, 16, 16);
     }
 }
