7ba5289c96a3a4b8a7fb2fae712642e74634e82e
RodoFire <louisbriche@gmail.com>
2024-07-04 17:48:11 +0200

    display changes for 1.20.4

--- src/main/java/net/rodofire/mushrooomsmod/compat/ForgeDisplay.java
+++ src/main/java/net/rodofire/mushrooomsmod/compat/ForgeDisplay.java
@@ -5,6 +5,7 @@ import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.recipe.RecipeEntry;
 import net.rodofire.mushrooomsmod.recipe.ForgeRecipe;
 
 import java.util.ArrayList;
@@ -12,13 +13,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class ForgeDisplay extends BasicDisplay {
+    private RecipeEntry<ForgeRecipe> recipe;
+
     public ForgeDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
         super(inputs, outputs);
     }
 
-    public ForgeDisplay(ForgeRecipe recipe) {
-        super(getInputList(recipe), List.of(EntryIngredient.of(EntryStacks.of(recipe.getOutput(null)))));
+    public ForgeDisplay(RecipeEntry<ForgeRecipe> recipe) {
+        super(getInputList(recipe.value()), List.of(EntryIngredient.of(EntryStacks.of(recipe.value().getResult(null)))));
+        this.recipe = recipe;
     }
+
 
 
     private static List<EntryIngredient> getInputList(ForgeRecipe recipe) {
