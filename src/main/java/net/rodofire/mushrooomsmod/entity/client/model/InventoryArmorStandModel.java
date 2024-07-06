35fc2ea893a4914a471471e2b5dddd0b78fad9e9
RodoFire <louisbriche@gmail.com>
2024-07-06 18:29:34 +0200

    model changes for 1.21

--- src/main/java/net/rodofire/mushrooomsmod/entity/client/model/InventoryArmorStandModel.java
+++ src/main/java/net/rodofire/mushrooomsmod/entity/client/model/InventoryArmorStandModel.java
@@ -8,16 +8,16 @@ import software.bernie.geckolib.model.GeoModel;
 public class InventoryArmorStandModel extends GeoModel<InventoryArmorStandEntity> {
     @Override
     public Identifier getModelResource(InventoryArmorStandEntity animatable) {
-        return new Identifier(MushrooomsMod.MOD_ID, "geo/inventory_armor_stand.geo.json");
+        return Identifier.of(MushrooomsMod.MOD_ID, "geo/inventory_armor_stand.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(InventoryArmorStandEntity animatable) {
-        return new Identifier(MushrooomsMod.MOD_ID, "textures/entity/inventory_armor_stand.png");
+        return Identifier.of(MushrooomsMod.MOD_ID, "textures/entity/inventory_armor_stand.png");
     }
 
     @Override
     public Identifier getAnimationResource(InventoryArmorStandEntity animatable) {
-        return new Identifier(MushrooomsMod.MOD_ID, "animations/inventory_armor_stand.animation.json");
+        return Identifier.of(MushrooomsMod.MOD_ID, "animations/inventory_armor_stand.animation.json");
     }
 }
