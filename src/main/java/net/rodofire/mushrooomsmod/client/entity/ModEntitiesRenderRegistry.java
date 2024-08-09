b39de505beef9de24dc4bcda1af993f3477bce45
RodoFire <louisbriche@gmail.com>
2024-08-09 14:14:10 +0200

    better organization

---
+++ src/main/java/net/rodofire/mushrooomsmod/client/entity/ModEntitiesRenderRegistry.java
@@ -0,0 +1,17 @@
+package net.rodofire.mushrooomsmod.client.entity;
+
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.rodofire.mushrooomsmod.entity.ModEntities;
+import net.rodofire.mushrooomsmod.entity.client.renderer.*;
+
+public class ModEntitiesRenderRegistry {
+    public static void registerEntities() {
+        EntityRendererRegistry.register(ModEntities.GROKI, GrokiRenderer::new);
+        EntityRendererRegistry.register(ModEntities.BOLETE_COW, BoleteCowRenderer::new);
+        EntityRendererRegistry.register(ModEntities.PLOTI, PlotiRenderer::new);
+        EntityRendererRegistry.register(ModEntities.CRYSTAL_CREEPER, CrystalCreeperRenderer::new);
+        EntityRendererRegistry.register(ModEntities.CRYSTAL_GOLEM, CrystalGolemRenderer::new);
+        EntityRendererRegistry.register(ModEntities.SCHROOM_STICK, SchroomStickRenderer::new);
+        EntityRendererRegistry.register(ModEntities.INVENTORY_ARMOR_STAND_ENTITY, InventoryArmorStandRenderer::new);
+    }
+}
---
+++ src/main/java/net/rodofire/mushrooomsmod/entity/ModEntitiesAttribute.java
@@ -0,0 +1,17 @@
+package net.rodofire.mushrooomsmod.entity;
+
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.LivingEntity;
+import net.rodofire.mushrooomsmod.entity.custom.*;
+
+public class ModEntitiesAttribute {
+    public static void registerAttributes() {
+        FabricDefaultAttributeRegistry.register(ModEntities.GROKI, GrokiEntity.setAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.BOLETE_COW, BoleteCowEntity.setAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.PLOTI, PlotiEntity.setAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.CRYSTAL_CREEPER, CustomCreeperEntity.createCreeperAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.CRYSTAL_GOLEM, CrystalGolemEntity.createCrystalGolemAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.SCHROOM_STICK, SchroomStickEntity.setAttributes());
+        FabricDefaultAttributeRegistry.register(ModEntities.INVENTORY_ARMOR_STAND_ENTITY, LivingEntity.createLivingAttributes());
+    }
+}
