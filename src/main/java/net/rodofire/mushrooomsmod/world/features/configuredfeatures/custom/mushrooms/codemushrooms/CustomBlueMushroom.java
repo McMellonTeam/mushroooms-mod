5df1c7e4cf39a3991048945877d4b98a3ec0002b
RodoFire <louisbriche@gmail.com>
2024-09-05 16:06:23 +0200

    blue mushroom tree changes

--- src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/mushrooms/codemushrooms/CustomBlueMushroom.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/mushrooms/codemushrooms/CustomBlueMushroom.java
@@ -15,8 +15,19 @@ public abstract class CustomBlueMushroom extends Feature<ModMushroomFeatureConfi
         super(configCodec);
     }
 
-    public boolean canGenerate(FeatureContext<ModMushroomFeatureConfig> context) {
-        return false;
+    public boolean canPlace(StructureWorldAccess world, Random random, BlockPos pos, int large, int height) {
+        for (int i = -large + 4; i <= large - 4; i++) {
+            for (int j = -height + 1; j <= height; j++) {
+                for (int k = -large + 2; k <= large - 2; k++) {
+                    BlockState state = world.getBlockState(pos.add(i, j, k));
+                    if (state.isAir()) {
+                        continue;
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
@@ -28,18 +39,22 @@ public abstract class CustomBlueMushroom extends Feature<ModMushroomFeatureConfi
         Random random = context.getRandom();
         BlockState cap = context.getConfig().capProvider.get(random, pos);
         BlockState trunk = context.getConfig().stemProvider.get(random, pos);
-        int height = Random.create().nextBetween(10, 15);
+        int height = Random.create().nextBetween(10, 20);
         int secondheight = 0;
         if (Random.create().nextBoolean()) {
             secondheight = Random.create().nextBetween(2, 5);
         }
+
         int minlarge = Random.create().nextBetween(2, 3);
         int maxlarge = Random.create().nextBetween(minlarge + 2, minlarge + 3);
+        int large = Random.create().nextBetween(maxlarge + 6, maxlarge + 10);
+        int heightCap = Random.create().nextBetween(3, maxlarge);
 
         if (!worldAccess.getBlockState(pos.down()).isIn(BlockTags.DIRT)) return false;
+        if (!this.canPlace(worldAccess, random, pos.add(0, height + secondheight, 0), large, heightCap)) return false;
 
-        this.generateLargeCap(worldAccess, random, pos.add(0, height + secondheight, 0), maxlarge, cap);
-        this.generateLargeTrunk(worldAccess, random, pos, trunk, false, height + secondheight, maxlarge, minlarge);
+        this.generateLargeCap(worldAccess, random, pos.add(0, height + secondheight, 0), maxlarge, cap, large, heightCap);
+        this.generateLargeTrunk(worldAccess, random, pos, trunk, false, height + secondheight - 1, maxlarge, minlarge);
 
 
         long endTimeCartesian = (System.nanoTime());
@@ -50,7 +65,7 @@ public abstract class CustomBlueMushroom extends Feature<ModMushroomFeatureConfi
     }
 
 
-    public abstract void generateLargeCap(StructureWorldAccess world, Random random, BlockPos pos, int maxlarge, BlockState state);
+    public abstract void generateLargeCap(StructureWorldAccess world, Random random, BlockPos pos, int maxlarge, BlockState state, int large, int height);
 
 
     public abstract void generateLargeTrunk(StructureWorldAccess world, Random random, BlockPos pos, BlockState trunk, boolean force, int height, int maxlarge, int minlarge);
