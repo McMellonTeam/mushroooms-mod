1c6cd11c72afe899b490f4d67159e15d8d36b1dd
RodoFire <louisbriche@gmail.com>
2024-09-08 14:59:24 +0200

    mixins for custom buttons on cloth config screen

---
+++ src/main/java/net/rodofire/mushrooomsmod/mixin/ClothConfigScreenMixin.java
@@ -0,0 +1,90 @@
+package net.rodofire.mushrooomsmod.mixin;
+
+import me.shedaniel.clothconfig2.gui.ClothConfigScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmLinkScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import net.rodofire.mushrooomsmod.MushrooomsMod;
+import net.rodofire.mushrooomsmod.client.hud.ImageButtonWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(ClothConfigScreen.class)
+public class ClothConfigScreenMixin {
+    //*/icons
+    @Unique
+    private static final Identifier DISCORD_ICON = new Identifier(MushrooomsMod.MOD_ID, "textures/gui/discord.png");
+    @Unique
+    private static final Identifier GITHUB_ICON = new Identifier(MushrooomsMod.MOD_ID, "textures/gui/github.png");
+    @Unique
+    private static final Identifier KOFI_ICON = new Identifier(MushrooomsMod.MOD_ID, "textures/gui/kofi.png");
+    @Unique
+    private static final Identifier CURSEFORGE_ICON = new Identifier(MushrooomsMod.MOD_ID, "textures/gui/curseforge.png");
+    @Unique
+    private static final Identifier MODRINTH_ICON = new Identifier(MushrooomsMod.MOD_ID, "textures/gui/modrinth.png");
+
+
+    //links
+    @Unique
+    private static final String DISCORD_LINK = "https://discord.gg/bAQRUxNyFj";
+    @Unique
+    private static final String GITHUB_LINK = "https://github.com/McMellonTeam/mushroooms-mod";
+    @Unique
+    private static final String KOFI_LINK = "https://ko-fi.com/rodofire";
+    @Unique
+    private static final String CURSEFORGE_LINK = "https://www.curseforge.com/minecraft/mc-mods/mushroooms";
+    @Unique
+    private static final String MODRINTH_LINK = "https://modrinth.com/mod/mushroooms";
+
+
+    @Inject(method = "init", at = @At("HEAD"))
+    private void addButtons(CallbackInfo ci) {
+        System.out.println(((Screen) (Object) this).getTitle().getString());
+        if (((Screen) (Object) this).getTitle().getString().equals("MushrooomsMod Options")) {
+            List<ImageButtonWidget> buttons = getButtons();
+            for (ImageButtonWidget button : buttons) {
+                ((ScreenMixin) this).invokeAddDrawableChild(button);
+            }
+        }
+    }
+
+    @Unique
+    public ImageButtonWidget createButton(String link, int yOffset, Identifier icon, Screen screen) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ImageButtonWidget discord = new ImageButtonWidget(
+                8, client.getWindow().getScaledHeight() / 2 + yOffset,
+                24, 24, icon,
+                button -> {
+                    MinecraftClient.getInstance().setScreen(new ConfirmLinkScreen(
+                            open -> {
+                                if (open) {
+                                    Util.getOperatingSystem().open(link);
+                                }
+                                System.out.println("last :" + screen.toString());
+                                MinecraftClient.getInstance().setScreen(screen);
+                            }, link, true)
+                    );
+                }
+        );
+        return discord;
+    }
+
+    @Unique
+    public List<ImageButtonWidget> getButtons() {
+        List<ImageButtonWidget> buttons = new ArrayList<>();
+        buttons.add(createButton(GITHUB_LINK, 28, GITHUB_ICON, (ClothConfigScreen) (Object) this));
+        buttons.add(createButton(DISCORD_LINK, 0, DISCORD_ICON, (ClothConfigScreen) (Object) this));
+        buttons.add(createButton(KOFI_LINK, 56, KOFI_ICON, (ClothConfigScreen) (Object) this));
+        buttons.add(createButton(CURSEFORGE_LINK, -28, CURSEFORGE_ICON, (ClothConfigScreen) (Object) this));
+        buttons.add(createButton(MODRINTH_LINK, -56, MODRINTH_ICON, (ClothConfigScreen) (Object) this));
+        return buttons;
+    }
+}
---
+++ src/main/java/net/rodofire/mushrooomsmod/mixin/ScreenMixin.java
@@ -0,0 +1,25 @@
+package net.rodofire.mushrooomsmod.mixin;
+
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Screen.class)
+public interface ScreenMixin {
+
+    /*@Invoker("addDrawableChild")
+    public <T extends Element & Drawable & Selectable> T addDrawableChild(T drawableElement) {
+        throw new UnsupportedOperationException();
+    }*/
+
+    @Invoker("addDrawableChild")
+    <T extends Element & Drawable & Selectable> T invokeAddDrawableChild(T drawableElement);
+
+    @Invoker("addDrawable")
+    <T extends Drawable> T invokeAddDrawable(T drawableElement);
+
+
+}
