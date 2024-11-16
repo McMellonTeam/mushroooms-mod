a7922153e00628dcc59dd8ce465e70566c4390ef
RodoFire <louisbriche@gmail.com>
2024-11-16 18:55:58 +0100

    changes related to ewc changes, as well as some fixes

--- src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/SpiralMushroom.java
+++ src/main/java/net/rodofire/mushrooomsmod/world/features/configuredfeatures/custom/SpiralMushroom.java
@@ -59,19 +59,15 @@ public class SpiralMushroom extends Feature<DefaultFeatureConfig> {
         spiral.setOutlineRadiusX(2);
 
         spiral.setRadiusX(new Pair<>(large, 1));
-        spiral.setRadiusX(new Pair<>(large, 1));
+        spiral.setRadiusZ(new Pair<>(large, 1));
 
-        spiral.setOffset(Random.create().nextBetween(0, 360));
+        spiral.setSpiralOffset(Random.create().nextBetween(0, 360));
         spiral.setYRotation(Random.create().nextBetween(-20, 20));
 
         List<Set<BlockPos>> posList = spiral.getBlockPos();
         if (!canPlace(world, posList)) return false;
 
-        try {
-            spiral.place(posList);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        spiral.place(posList);
 
         return true;
     }
